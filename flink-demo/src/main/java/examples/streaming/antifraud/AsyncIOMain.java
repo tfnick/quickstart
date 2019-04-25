0ecd027d467f8a8e4274e84dff6e69436b310ab6
Administrator <admin@cafintech.com>
2019-04-25 20:35:36 +0800

    commit

--- flink-demo/src/main/java/examples/streaming/antifraud/AsyncIOMain.java
+++ flink-demo/src/main/java/examples/streaming/antifraud/AsyncIOMain.java
@@ -1,19 +1,23 @@
 package examples.streaming.antifraud;
 
+import examples.streaming.antifraud.function.AsyncHbaseRequestFunction;
 import examples.streaming.antifraud.model.ApplyMessage;
 import examples.streaming.antifraud.source.MockMessageSource;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.Tuple3;
 
 /**
  * Use AsyncIO to enrich data attributes
  */
 public class AsyncIOMain {
 
-
+        //https://www.liangzl.com/get-article-detail-114587.html
     private static final Logger LOGGER = LoggerFactory.getLogger(StreamMain.class);
 
     public static void main(String[] args) throws Exception {
@@ -23,7 +27,17 @@ public class AsyncIOMain {
         env.getConfig().setGlobalJobParameters(ParameterTool.fromArgs(args));
         DataStreamSource<ApplyMessage> applyStream = env.addSource(new MockMessageSource());
 
+        //110123199010025771 (idCard)
+        applyStream.map(new MapFunction<ApplyMessage, Tuple4<String, Integer, Integer, Double>>() {
+            @Override
+            public Tuple4<String, Integer, Integer, Double> map(ApplyMessage applyMessage) throws Exception {
+                String idCard = applyMessage.getIdCard();
+                Double money = applyMessage.getMoney();
 
+
+                return null;
+            }
+        });
 
                 //env.execute("x");
 
--- flink-demo/src/main/java/examples/streaming/antifraud/function/AsyncHbaseRequestFunction.java
+++ flink-demo/src/main/java/examples/streaming/antifraud/function/AsyncHbaseRequestFunction.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 /**
  * 演示flink通过Async IO Function接口实现异步访问数据库或者Http服务
  */
-public class AsyncDbRequestFunction extends RichAsyncFunction<String, Tuple3<String,Integer,Integer>> {
+public class AsyncHbaseRequestFunction extends RichAsyncFunction<String, Tuple3<String,Integer,Integer>> {
 
     //async hbase client The client is compatible with HBase from versions 0.92 to 0.94 and 0.96 to 1.3.
     HBaseClient client;
@@ -25,7 +25,7 @@ public class AsyncDbRequestFunction extends RichAsyncFunction<String, Tuple3<Str
     public void open(Configuration parameters) throws Exception {
         super.open(parameters);
         Config config = new Config();
-        config.getMap().put("hbase.zookeeper.quorum", "192.168.1.1:2181,192.168.1.2:2181");
+        config.getMap().put("hbase.zookeeper.quorum", "192.168.1.168:2181");
         config.getMap().put("hbase.zookeeper.session.timeout", "5000");
         client = new HBaseClient(config);
     }
@@ -41,7 +41,7 @@ public class AsyncDbRequestFunction extends RichAsyncFunction<String, Tuple3<Str
     @Override
     public void asyncInvoke(String idCard, ResultFuture<Tuple3<String, Integer, Integer>> resultFuture) throws Exception {
         //query table idcard(id,x2,x3)
-        GetRequest getRequest = new GetRequest("gr_idcard", idCard, "df");
+        GetRequest getRequest = new GetRequest("id_card", idCard, "df");
 
         client.get(getRequest).addCallback(new Callback<Tuple3<String, Integer, Integer>, ArrayList<KeyValue>>(){
             @Override
